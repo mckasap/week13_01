fe58afca2eae010e0374f4874ad2b332fc00f21b
mckasapbasi <mckasap@gmail.com>
2023-05-10 10:46:22 +0300

    ikinci kommit

--- app/src/main/java/com/example/week13_01/MainActivity.java
+++ app/src/main/java/com/example/week13_01/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.week13_01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +20,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+boolean swtc=false;
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+    int id= item.getItemId();
+
+    switch(id){
+
+        case  R.id.about: Toast.makeText(this,"about",Toast.LENGTH_LONG).show();
+            break;
+
+        case R.id.help: Toast.makeText(this,"help",Toast.LENGTH_LONG).show();
+            break;
+
+    }
+
+        return super.onOptionsItemSelected(item);
+    }
+Switch switchWidget;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inf= getMenuInflater();
+        inf.inflate(R.menu.mymenu,menu);
+
+
+        MenuItem menuItem = menu.findItem(R.id.app_bar_switch);
+        switchWidget = (Switch) menuItem.getActionView();
+        switchWidget.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Switch is ON
+                    Toast.makeText(MainActivity.this,"ONN",Toast.LENGTH_LONG).show();
+                } else {
+                    // Switch is OFF
+                    Toast.makeText(MainActivity.this,"OFF",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void myClick(View v){
@@ -25,9 +75,12 @@ public class MainActivity extends AppCompatActivity {
 
         int ident= getResources().getIdentifier(str,"raw","com.example.week13_01");
 
-       
+
         MediaPlayer mp = MediaPlayer.create(this,ident);
         mp.start();
 
     }
+
+
+
 }
